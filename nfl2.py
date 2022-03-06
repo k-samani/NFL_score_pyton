f4dd1c066c1c07a362156006296a143bb7fd133c
ksamani <ksamani25@gmail.com>
2022-03-06 02:51:25 -0600

    updated

--- nfl2.py
+++ nfl2.py
@@ -10,10 +10,10 @@ def Get_Source(link):
         f.write(req.text)
         f.close()
     
-    return req.text
+    return #req.text
 
 
-parsed_source = Get_Source('http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard')
+Get_Source('http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard')
 
 the_list = {}
 
@@ -23,19 +23,25 @@ with open('html_source.txt', 'r') as f:
 for i in nfl_data['events']:
     #print(i['name'])
     for k in i['competitions']:
+        print(k)
         for l in k['competitors']:
            the_list[l['team']['displayName']] = l['score']
 
+
+
+print(the_list)
+print()
+print()
 
 
 x=0
 for key in the_list:
     if x < 1:
         print()
-        print(key + " " + the_list[key] + " vs ")
+        print('Home: ' + key + " " + the_list[key])
         x = x + 1
     else:
-        print(key + " " + the_list[key])
+        print('Away: ' + key + " " + the_list[key])
         x = 0
         print()
     
